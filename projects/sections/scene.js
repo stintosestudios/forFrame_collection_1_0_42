fcf2f29c0ab927a24780e36d4f62dd46e92468c5
stintosestudios <stintose.studios@gmail.com>
2016-12-25 01:26:16 -0500

    sections looking good

--- projects/sections/scene.js
+++ projects/sections/scene.js
@@ -3,7 +3,7 @@ console.log('scene.js for sections');
 // start by setting up the scene
 scene({
 
-    maxFrame : 50,
+    maxFrame : 100,
 
     // define some parts
     parts : [{
@@ -27,11 +27,11 @@ scene({
     sections : {
 
         // the section timeline
-        timeline : 'zoomout:50;zoomin:100',
+        timeline : 'cout:20;swag:40;err:60;over:80;cin:100',
 
         forFrame : {
 
-            zoomout : function () {
+            cout : function () {
 
                 var part = this.parts['box'];
 
@@ -40,10 +40,54 @@ scene({
 
                 part.x = 320 - part.w / 2;
                 part.y = 240 - part.h / 2;
+                part.radian = 0;
 
             },
 
-            zoomin : function () {
+            swag : function () {
+
+                var part = this.parts['box'],
+                bias = Math.abs(.5 - this.sectionPer) / .5;
+
+                part.w = 640 * this.sectionPer;
+                part.h = 480 * this.sectionPer;
+
+                part.x = 640 * this.sectionPer;
+                part.y = 240 - part.h / 2 * bias;
+                part.radian = Math.PI * this.sectionPer;
+
+            },
+
+            err : function () {
+
+                var part = this.parts['box'],
+                bias = Math.abs(.5 - this.sectionPer) / .5;
+
+                part.w = 640 - 640 * this.sectionPer;
+                part.h = 480 - 480 * this.sectionPer;
+
+                part.x = 640 - 640 * this.sectionPer;
+                part.y = 240 - part.h / 2 * bias;
+                part.radian = Math.PI * 3 * this.sectionPer;
+
+            },
+
+            over : function () {
+
+                var part = this.parts['box'],
+
+                bias = Math.abs(.5 - this.sectionPer) / .5;
+
+                part.w = 480 - 480 * bias;
+                part.h = 480 - 480 * bias;
+
+                part.x = 640 * this.sectionPer - part.w / 2;
+                part.y = 240 - part.h / 2;
+                part.radian = 0;
+
+            },
+
+            cin : function () {
 
                 var part = this.parts['box'];
 
@@ -53,6 +97,7 @@ scene({
                 part.x = 320 - part.w / 2;
                 part.y = 240 - part.h / 2;
 
+                part.radian = 0;
             }
 
         }
